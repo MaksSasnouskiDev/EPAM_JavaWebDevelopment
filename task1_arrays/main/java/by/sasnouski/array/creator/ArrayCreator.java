92a976defc72400e7bdfd24194da1849d2c43bac
MaksSki <88492472+MaksSasnouskiDev@users.noreply.github.com>
2022-02-08 00:55:38 +0300

    Delete task1_arrays directory

--- task1_arrays/main/java/by/sasnouski/array/creator/ArrayCreator.java
+++
@@ -1,9 +0,0 @@
-package by.sasnouski.array.creator;
-
-import by.sasnouski.array.entity.ArrayOfNumbers;
-import by.sasnouski.array.parser.StringParser;
-import static by.sasnouski.array.reader.ReadDataFromFile.readData;
-
-public class ArrayCreator {
-    ArrayOfNumbers array=new ArrayOfNumbers(StringParser.createNumbersArray());
-}
