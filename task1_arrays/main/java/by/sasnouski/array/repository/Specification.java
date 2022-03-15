5b4dc6bd55b94945ade3884ae1c815c918d4c6bb
MaksSki <88492472+MaksSasnouskiDev@users.noreply.github.com>
2022-03-15 09:05:26 +0300

    Delete task1_arrays directory

--- task1_arrays/main/java/by/sasnouski/array/repository/Specification.java
+++
@@ -1,9 +0,0 @@
-package by.sasnouski.array.repository;
-
-
-import by.sasnouski.array.entity.ListsOfNumbers;
-
-public interface Specification {
-    boolean specify(ListsOfNumbers customArray);
-
-}
