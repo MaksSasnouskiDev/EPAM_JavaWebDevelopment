6d5a9ebe2804e3c74e7a2284530811e5eb0104de
MaksSki <88492472+MaksSasnouskiDev@users.noreply.github.com>
2022-03-22 06:30:21 +0300

    Delete task3_text_handling_composite directory

--- task3_text_handling_composite/java/by/sasnouski/text_handling/parser/TextParserInterface.java
+++
@@ -1,8 +0,0 @@
-package by.sasnouski.text_handling.parser;
-
-import by.sasnouski.text_handling.entity.TextComposite;
-
-public interface TextParserInterface {
-
-    TextComposite parseText(String text);
-}
